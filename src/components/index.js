5854a8b5c6f2dd75546c8121b99b40be361dd519
Astafiev Maksim <maxast0802@yandex.ru>
2024-09-08 13:46:13 +0300

    фин

--- src/components/index.js
+++ src/components/index.js
@@ -13,7 +13,7 @@ const cardlist = document.querySelector('.places__list');
 const buttonEditProfile = document.querySelector('.profile__edit-button');
 const buttonAddProfile = document.querySelector('.profile__add-button');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
-const popupContentImage = document.querySelector('.popup__content_content_image')
+const popupTypeImage = document.querySelector('.popup_type_image');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_description');
 const popupNewCard = document.querySelector('.popup_type_new-card');
@@ -35,7 +35,7 @@ buttonEditProfile.addEventListener('click', () => {
 
 // открытие изображения 
 function openModalImage(evt) { 
-    openModal (popupContentImage); // функция   openModal должна быть преобразована по образцу в modal.js. 
+    openModal (popupTypeImage); // функция   openModal должна быть преобразована по образцу в modal.js. 
     const imagePopup = document.querySelector('.popup__image'); 
     const popupCaption = document.querySelector('.popup__caption'); 
     imagePopup.src = evt.target.src; 
